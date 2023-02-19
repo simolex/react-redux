ac1f7483b5cf77d0def7794f6a8ec17178da4028
simolex <simolex@yandex.ru>
2023-02-19 22:53:01 +0300

    create state, reducer and selector

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+    const dispatch = useDispatch();
+    const cash = useSelector((state) => state.cash);
+
+    const addCash = (cash) => {
+        dispatch({ type: "ADD_CASH", payload: cash });
+    };
+
+    const getCash = (cash) => {
+        dispatch({ type: "GET_CASH", payload: cash });
+    };
+
+    return (
+        <div className="App">
+            <div style={{ fontSize: "3rem" }}>{cash}</div>
+            <div style={{ display: "flex" }}>
+                <button onClick={() => getCash(+prompt())}>Снять со счета</button>
+                <button onClick={() => addCash(+prompt())}>Пополнить счет</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
